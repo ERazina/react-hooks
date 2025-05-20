b73b48186fafa63398195fdea2cffbaa49f72633
Elina Razina <e.razina@samolet.ru>
2025-05-21 00:44:44 +0700

    useFetch get

---
+++ src/useFetch.ts
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+
+const useFetch = (url: string) => {
+  const [data, setData] = useState<any | null>(null);
+  const [error, setError] = useState<Error | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch(url);
+        if (!response.ok) {
+          console.error(`HTTP error! Status: ${response.status}`);
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.json();
+        setData(data);
+      } catch (e: unknown) {
+        if (e instanceof Error) {
+          console.error(e.name, e.message);
+          setError(e);
+        } else {
+          setError(new Error("unknown error"));
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data, error, loading };
+};
+
+export default useFetch;
